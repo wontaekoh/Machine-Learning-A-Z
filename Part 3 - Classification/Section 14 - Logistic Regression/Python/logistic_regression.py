4e1451cd4f82dfc19e921a2f4bd8717f1012fef6
wontaekoh <wontaek.oh.5@gmail.com>
2021-06-09 22:22:52 -0400

    Regression done

--- Part 3 - Classification/Section 14 - Logistic Regression/Python/logistic_regression.py
+++ Part 3 - Classification/Section 14 - Logistic Regression/Python/logistic_regression.py
@@ -1,16 +1,18 @@
-# Logistic Regression
+# # Logistic Regression
 
-# Importing the libraries
+""" Importing the libraries"""
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+
+""" Importing the dataset"""
 dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-# Splitting the dataset into the Training set and Test set
+
+""" Splitting the dataset into the Training set and Test set"""
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 print(X_train)
@@ -18,7 +20,8 @@ print(y_train)
 print(X_test)
 print(y_test)
 
-# Feature Scaling
+
+""" Feature Scaling"""
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
@@ -26,25 +29,30 @@ X_test = sc.transform(X_test)
 print(X_train)
 print(X_test)
 
-# Training the Logistic Regression model on the Training set
+
+""" Training the Logistic Regression model on the Training set"""
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
 
-# Predicting a new result
+
+""" Predicting a new result"""
 print(classifier.predict(sc.transform([[30,87000]])))
 
-# Predicting the Test set results
+
+""" Predicting the Test set results"""
 y_pred = classifier.predict(X_test)
 print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 
-# Making the Confusion Matrix
+
+""" Making the Confusion Matrix"""
 from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 accuracy_score(y_test, y_pred)
 
-# Visualising the Training set results
+
+""" Visualising the Training set results"""
 from matplotlib.colors import ListedColormap
 X_set, y_set = sc.inverse_transform(X_train), y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
@@ -61,7 +69,8 @@ plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
 
-# Visualising the Test set results
+
+""" Visualising the Test set results"""
 from matplotlib.colors import ListedColormap
 X_set, y_set = sc.inverse_transform(X_test), y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
