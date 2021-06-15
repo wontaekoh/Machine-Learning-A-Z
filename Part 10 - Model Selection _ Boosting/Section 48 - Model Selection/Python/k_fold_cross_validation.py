48b3b11ef6061008ded464915f466ba85b4a4089
wontaekoh <wontaek.oh.5@gmail.com>
2021-06-14 23:01:46 -0400

    Logistic Regression start

--- Part 10 - Model Selection _ Boosting/Section 48 - Model Selection/Python/k_fold_cross_validation.py
+++ Part 10 - Model Selection _ Boosting/Section 48 - Model Selection/Python/k_fold_cross_validation.py
@@ -1,44 +1,51 @@
-# k-Fold Cross Validation
+# # k-Fold Cross Validation
 
-# Importing the libraries
+""" Importing the libraries"""
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+
+""" Importing the dataset"""
 dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-# Splitting the dataset into the Training set and Test set
+
+""" Splitting the dataset into the Training set and Test set"""
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
-# Feature Scaling
+
+""" Feature Scaling"""
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Training the Kernel SVM model on the Training set
+
+""" Training the Kernel SVM model on the Training set"""
 from sklearn.svm import SVC
 classifier = SVC(kernel = 'rbf', random_state = 0)
 classifier.fit(X_train, y_train)
 
-# Making the Confusion Matrix
+
+""" Making the Confusion Matrix"""
 from sklearn.metrics import confusion_matrix, accuracy_score
 y_pred = classifier.predict(X_test)
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 accuracy_score(y_test, y_pred)
 
-# Applying k-Fold Cross Validation
+
+""" Applying k-Fold Cross Validation"""
 from sklearn.model_selection import cross_val_score
 accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
 print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
 print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
 
-# Visualising the Training set results
+
+""" Visualising the Training set results"""
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
@@ -56,7 +63,8 @@ plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
 
-# Visualising the Test set results
+
+""" Visualising the Test set results"""
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
